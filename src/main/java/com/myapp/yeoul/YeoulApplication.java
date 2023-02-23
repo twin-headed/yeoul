f5780d991e95cab835d984be69bd3570170dd60b
trionis <trionis@naver.com>
2023-02-23 14:08:02 +0900

    first commmit

---
+++ src/main/java/com/myapp/yeoul/YeoulApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.yeoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeoulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/yeoul/YeoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.yeoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
