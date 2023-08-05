9f9dbabd5af313576718f1fcf780ff0953a85e0e
trionis <trionis@naver.com>
2023-08-05 10:52:20 +0900

    0707

--- src/main/java/com/myapp/controller/FileController.java
+++ src/main/java/com/myapp/controller/FileController.java
@@ -1,6 +1,7 @@
 package com.myapp.controller;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,28 +21,30 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
  */
 @Controller
 public class FileController {
-	
+
 	@Autowired
 	private AmazonS3Client amazonS3Client;
-	
+
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
 	@ResponseBody
 	public String upload (@RequestParam("file") MultipartFile multipartFile) throws IOException{
+		System.out.println(multipartFile);
 		String originalName = multipartFile.getOriginalFilename(); // 파일 이름
+		String fileName = UUID.randomUUID() + originalName;
 		long size = multipartFile.getSize(); // 파일 크기
-		
+
 		ObjectMetadata objectMetaData = new ObjectMetadata();
 		objectMetaData.setContentType(multipartFile.getContentType());
 		objectMetaData.setContentLength(size);
-		
+
 		// S3에 업로드
 		amazonS3Client.putObject(
-			new PutObjectRequest("yeoulserver", originalName, multipartFile.getInputStream(), objectMetaData)
-				.withCannedAcl(CannedAccessControlList.PublicRead)
+				new PutObjectRequest("yeoulserver", fileName, multipartFile.getInputStream(), objectMetaData)
+						.withCannedAcl(CannedAccessControlList.PublicRead)
 		);
-		
+
 		String imagePath = amazonS3Client.getUrl("yeoulserver", originalName).toString(); // 접근가능한 URL 가져오기
         return imagePath;
 	}
-	
+
 }
